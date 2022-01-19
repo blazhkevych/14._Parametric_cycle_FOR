3b351f0c3aa6e1493dfd8dc9bf721ac34f68329a
Виктор Блажкевич <blazhkevych.va@gmail.com>
2022-01-19 21:33:30 +0200

    Add project files.

---
+++ task_1/task_1.cpp
@@ -0,0 +1,18 @@
+﻿/*
+1. Вывести на экран шахматную доску, используя пробел и символ с
+кодом 219.
+*/
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	setlocale(LC_ALL, ""); // работает только с потоком вывода
+
+
+
+	return 0;
+}
