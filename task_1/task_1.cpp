002f07df7a352d5315c70c4e9db2e5f7856b985e
Виктор Блажкевич <blazhkevych.va@gmail.com>
2022-01-20 12:47:09 +0200

    правки

--- task_1/task_1.cpp
+++ task_1/task_1.cpp
@@ -12,7 +12,35 @@ int main()
 {
 	setlocale(LC_ALL, ""); // работает только с потоком вывода
 
+	cout << "Введите высоту: " << endl;
+	int side{ 0 };
+	cin >> side;
 
+	int i{ 0 }, j{ 0 };
+
+	for (i = 0; i < side; i++)
+	{
+		for (j = 0; j < side; j++)
+		{
+			if (i == j
+				)
+			{
+				int symbol = 219;
+				cout << (char)symbol;
+			}
+			else
+				cout << "  ";
+		}
+		cout << "\n";
+	}
 
 	return 0;
 }
+//
+//int main()
+//{
+//	int code;
+//	cin >> code;
+//	cout << (char)code;
+//	return 0;
+//}
