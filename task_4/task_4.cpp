ea08abaa287a268a4e6de016cceb803eb499551a
Виктор Блажкевич <blazhkevych.va@gmail.com>
2022-01-27 19:35:06 +0200

    правки

--- task_4/task_4.cpp
+++ task_4/task_4.cpp
@@ -5,6 +5,8 @@
 числа, с которого цепочка началась.
 */
 
+// В задании один цикл fot, а внтури ифы, больше ничего.
+
 #include <iostream>
 using std::cout;
 using std::cin;
@@ -14,6 +16,26 @@ int main()
 {
 	setlocale(LC_ALL, ""); // работает только с потоком вывода
 
+	cout << "Введите 15 целых чисел:" << endl;
+
+	int number{ 0 }, temp{ 0 }, position{ 0 }, start_position{ 0 }, finish_position{ 0 }, count{ 0 };
+
+	for (int i = 0; i < 15; i++)
+	{
+		cin >> number;
+
+		int temp = number;
+
+		if (temp >= number)
+		{
+			start_position = 15-i;
+			count++;
+
+		}
+	}
+	cout << "Максимальная длина цепочки: " <<count << endl;
+	cout << "порядковый номер того
+		числа, с которого цепочка началась : " <<count << endl;
 
 
 	return 0;
